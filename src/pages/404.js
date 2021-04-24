2f53e941da3348167802352c6ff5445eb0b51367
Praveen Singh <praveenkumar_singh@intuit.com>
2021-04-25 02:23:35 +0530

    First commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Layout } from 'components/common';
+import { Layout } from 'components/theme/Layout';
 
 export default () => (
     <Layout>
-        {/* <SEO title="404: Not found" location="/404" /> */}
         <h1>NOT FOUND</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
