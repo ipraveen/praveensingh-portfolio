4373362c396f7a9277e521cb917e235747acc776
Praveen Singh <praveenkumar_singh@intuit.com>
2022-05-18 18:45:35 +0530

    fix: avatar bodrer

--- src/components/panels/avatar/Avatar.jsx
+++ src/components/panels/avatar/Avatar.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import dev from 'assets/images/praveen.png';
 import * as styles from './avatar.module.scss';
-import RadialFrame from 'components/frames/radial/RadialFrame';
 
 const Avatar = () => {
     return (
-        <RadialFrame radius={130} color="white" strokeWidth = '2' strokeDashArray = '2,55'>
-            <div className={styles.avatar}>
-                <img src={dev} alt="Praveen Singh" />
-            </div>
-        </RadialFrame>
+        <div className={styles.avatar}>
+            <img src={dev} alt="Praveen Singh" />
+        </div>
     );
 };
 
