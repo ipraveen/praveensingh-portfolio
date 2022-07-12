c13495e306f18d1124ad6acb895c4dfb6084b7b0
Praveen Singh <praveenkumar_singh@intuit.com>
2022-07-12 09:28:34 +0530

    updating seo

--- src/components/common/seo/SEO.jsx
+++ src/components/common/seo/SEO.jsx
@@ -1,43 +1,26 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Thumbnail from 'assets/thumbnail/thumbnail.png';
-import {
-    url,
-    defaultDescription,
-    social,
-    defaultTitle,
-    socialLinks,
-    address,
-    contact,
-    legalName,
-    foundingDate,
-    logo,
-} from 'config/config';
+import siteIcon from 'assets/images/praveen.png';
+import { url, defaultDescription, defaultTitle } from 'config/config';
 
 export const SEO = ({ title = defaultTitle, description = defaultDescription }) => {
     return (
         <Helmet>
+            <title>{title}</title>
+            <html lang="en" dir="ltr" />
+
             <meta name="description" content={description} />
-            <meta name="image" content={Thumbnail} />
+            <meta name="image" content={siteIcon} />
+
+            {/**  COLOR SCHEME */}
             <meta name="color-scheme" content="dark light" />
 
-            {/* <meta property="og:url" content={`${url}${location}/?ref=smakosh.com`} />
+            {/**  OPEN GRAPH PROTOCOL */}
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={Thumbnail} />
-            <meta property="fb:app_id" content={social.facebook} /> */}
-
-            {/* <meta name="twitter:card" content="summary" />
-            <meta name="twitter:creator" content={socialLinks.twitter} />
-            <meta name="twitter:site" content={social.twitter} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image:src" content={Thumbnail} />
-            <script type="application/ld+json">{structuredDataOrganization}</script> */}
-            {/* <link rel="publisher" href={socialLinks.google} /> */}
-            <title>{title}</title>
-            <html lang="en" dir="ltr" />
+            <meta property="og:image" content={siteIcon} />
+            <meta property="og:url" content={url} />
         </Helmet>
     );
 };
--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     defaultTitle: 'Praveen Singh: full-stack JavaScript Engineer and Web Application Architect | portfolio | Blog',
     author: 'Praveen Singh',
+    url: 'https://praveensingh.net/',
     socialLinks: {
         twitter: 'https://twitter.com/i_praveensingh',
         github: 'https://github.com/ipraveen',
