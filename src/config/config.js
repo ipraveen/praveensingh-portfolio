40ee775a5c5b807e61c3ea2b87c4ef912d723a06
Praveen Singh <praveenkumar_singh@intuit.com>
2022-08-14 21:36:09 +0530

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,6 @@
 module.exports = {
     defaultTitle: 'Praveen Singh: full-stack JavaScript Engineer and Web Application Architect | portfolio | Blog',
-    defaultDescription: `I'm a <b>Full Stack Engineer</b> with the passion for Frontend Development and large scale System Design. I have been developing, highly scalable, data-driven web
-    applications professionally for <b>15+</b> years and led multiple full-size development
-    teams as a Tech Lead.`,
+    defaultDescription: `I'm a <b>Full Stack Engineer</b> with a passion for Frontend Development and large-scale System Design. I have been developing highly scalable, data-driven web applications for <b>15+</b> years and have led multiple full-size development teams as a Tech Lead.`,
     author: 'Praveen Singh',
     url: 'https://praveensingh.net/',
     socialLinks: {
