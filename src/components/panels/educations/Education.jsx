3b963dfa819a75583c11d985a6f3e9f6e08ee9c5
psingh7 <praveenkumar_singh@intuit.com>
2022-11-14 00:48:59 +0530

    minor

--- src/components/panels/educations/Education.jsx
+++ src/components/panels/educations/Education.jsx
@@ -12,14 +12,14 @@ const Education = () => {
                         <h1>Master of Science (MS)</h1>
                         <h2>Software System</h2>
                         <img src={bits} alt="bits"></img>
-                        <h3>Birla Institute of Technology and Science (BITS), Pilani</h3>
+                        <h4>Birla Institute of Technology and Science (BITS)<br/>Pilani</h4>
                         <h3>2011-2013</h3>
                     </section>
                     <section className={styles.item}>
                         <h1>Bachelor of Technology (B.Tech.)</h1>
                         <h2>Electronics and communication</h2>
                         <img src={jiit} alt="jiit"></img>
-                        <h3>Jaypee Institute of Information Technology (JIIT), Noida</h3>
+                        <h3>Jaypee Institute of Information Technology (JIIT)<br></br> Noida</h3>
                         <h3>2003-2007</h3>
                     </section>
                 </div>
