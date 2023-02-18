669417f5b60b15782c5f354780379f271f295150
Praveen Singh <praveensingh.name@gmail.com>
2023-02-18 15:28:40 +0530

    more fix

--- src/components/panels/avatar/Avatar.jsx
+++ src/components/panels/avatar/Avatar.jsx
@@ -6,8 +6,8 @@ const Avatar = ({ size = 'lg' }) => {
     return (
         <div size={size}>
             <div
-                className="block mx-auto rounded-full sm:mx-0 sm:shrink-0 outline  outline-slate-100 outline outline-offset-1"
-                css={[size === 'sm' ? tw`h-10` : tw`h-48 outline-4 outline-offset-1 outline-pink-500`]}
+                className="block mx-auto rounded-full sm:mx-0 sm:shrink-0  outline-slate-100 outline outline-offset-1"
+                css={[size === 'sm' ? tw`h-10` : tw`h-48 outline-offset-1 outline-pink-500`]}
                 size={size}
                 src={dev}
                 alt="Praveen Singh"
