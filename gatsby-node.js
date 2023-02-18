845356b621b8da11905e899c4b4bdff9532fc601
Praveen Singh <praveensingh.name@gmail.com>
2023-02-18 14:56:12 +0530

    push

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,13 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
         resolve: {
             modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+            alias: {
+                "components": path.resolve(__dirname, "src/components"),
+                "config": path.resolve(__dirname, "src/config"),
+                "hooks": path.resolve(__dirname, "src/hooks"),
+                "assets": path.resolve(__dirname, "src/assets"),
+              }
         },
+        
     });
 };
