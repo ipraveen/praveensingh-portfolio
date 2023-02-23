ce14218873bc431022c2102869d4129babd755ae
Praveen Singh <praveensingh.name@gmail.com>
2023-02-24 01:54:58 +0530

    some text

--- src/components/panels/avatar/Avatar.tsx
+++ src/components/panels/avatar/Avatar.tsx
@@ -3,7 +3,7 @@ import dev from 'assets/images/praveen.png';
 
 const Avatar = ({ className, size = 'lg' }) => {
     return (
-        <div className={`${className}` }>
+        <div className={`${className} flex flex-col justify-center items-center` }>
             <img
                 className={`${
                     size === 'sm' ? 'h-10 outline-1 outline-offset-0' : 'h-36 outline-4 outline-offset-4'
@@ -11,6 +11,15 @@ const Avatar = ({ className, size = 'lg' }) => {
                 src={dev}
                 alt="Praveen Singh"
             />
+            {
+                size === 'lg' &&(
+                    <div className='mt-4 flex flex-col justify-center items-center'>
+                        <small className='text-slate-300 text-xs'>Principle Engineer, Web Development</small>
+                        <small className='text-slate-300 text-xs'>M.S. Computer Science & B.Tech Electronics</small>
+                    </div>)
+            }
+            
+
         </div>
     );
 };
