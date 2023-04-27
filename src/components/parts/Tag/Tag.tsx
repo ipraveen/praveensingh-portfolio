61a51f154949b56d35a335771f1c5d64f901185d
Praveen Singh <praveensingh.name@gmail.com>
2023-04-28 01:55:03 +0530

    indeeddb article

--- src/components/parts/Tag/Tag.tsx
+++ src/components/parts/Tag/Tag.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.small`
-    background-color: #e6f4ea;
-    color: #0d652d;
+    /* background-color: #e6f4ea;
+    color: #0d652d; */
     /* background-color: #fef7e0;
     color: #b05a00; */
+    background-color: #e1ecf4fc;
+    color: #39739df5;
     border-radius: 0.3rem;
     font-size: ${({variant}) => variant === 'small'? '11px': '.8rem'};
     vertical-align: middle;
