eb936c60af0d2f143097b5736ae1f523792fcadd
Praveen Kumar Singh <pravsingh@twilio.com>
2025-08-05 23:00:29 +0530

    Updating Image

--- src/components/panels/avatar/Avatar.tsx
+++ src/components/panels/avatar/Avatar.tsx
@@ -8,7 +8,7 @@ const Avatar = ({ className, size = 'lg' }) => {
                 className={`${
                     size === 'sm' ? 'w-10 h-10 outline-1 outline-offset-0' : 'w-36 h-36 outline-4 outline-offset-4'
                 } outline outline-slate-300 rounded-full sm:mx-0 sm:shrink-0  block mx-auto brightness-110 hover:brightness-110`}
-                src="./images/praveen.png"
+                src="./images/praveen.jpeg"
                 alt="Praveen Singh"
             />
 
